c778c83fb4130334b048cd1c9661672f7ca62a7d
Andi <spam01@zreptil.de>
2018-02-01 06:35:03 +0100

    updated bower-components

--- server.bat
+++ server.bat
@@ -1,1 +1,3 @@
-start cmd /k polymer serve --port 3000 --hostname diamant.mine.de
+start cmd /c polymer serve --port 3000 --hostname diamant.mine.de
+start http://localhost:3000
+start notepad++ src\diamant-app\diamant-daydata.html
